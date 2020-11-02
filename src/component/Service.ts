345684b7e6689e95dc3d4740faa4f724b271c721
Henrique Martins <hmodinos@gmail.com>
2020-11-02 20:28:09 +0100

    Update Service.ts

--- src/component/Service.ts
+++ src/component/Service.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {Gate} from "./types";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://ec2-52-57-77-14.eu-central-1.compute.amazonaws.com:8080',
   timeout: 1000,
 });
 
